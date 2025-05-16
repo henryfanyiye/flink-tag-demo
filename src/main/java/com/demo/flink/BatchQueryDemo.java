fe0a0de7538ac703f17a9e6d6394da0fbbc25780
henry <henry.fan@softtek.com>
2025-05-16 18:10:52 +0800

    update

--- src/main/java/com/demo/flink/BatchQueryDemo.java
+++ src/main/java/com/demo/flink/BatchQueryDemo.java
@@ -5,9 +5,15 @@ import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.connector.jdbc.core.datastream.source.JdbcSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.table.api.TableResult;
+import org.apache.flink.types.Row;
+import org.apache.flink.util.CloseableIterator;
 
 /**
  * Flink JDBC 批处理示例，读取 MySQL 表数据
+ *
  * @author henry.fan
  */
 public class BatchQueryDemo {
@@ -37,6 +43,10 @@ public class BatchQueryDemo {
     }
 
     public static void main(String[] args) throws Exception {
+        TableQueryDemo();
+    }
+
+    public static void DataStreamQueryDemo() throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         // 构造 JDBC Source
@@ -47,6 +57,7 @@ public class BatchQueryDemo {
                 .setPassword("12345678")
                 .setSql("SELECT id, user_id, event_type, event_time FROM user_events")
                 .setResultSetType(java.sql.ResultSet.TYPE_FORWARD_ONLY)
+                .setSplitReaderFetchBatchSize(1)
                 .setResultExtractor(rs ->
                         new Event(
                                 rs.getInt("id"),
@@ -65,5 +76,32 @@ public class BatchQueryDemo {
                 .name("Print-To-Console");
 
         env.execute("Flink JDBC Batch Read Demo");
+    }
+
+    public static void TableQueryDemo() throws Exception {
+        EnvironmentSettings settings = EnvironmentSettings.inStreamingMode();
+        TableEnvironment tableEnv = TableEnvironment.create(settings);
+
+        tableEnv.executeSql(
+                "CREATE TABLE user_events (\n" +
+                        "  id INT,\n" +
+                        "  user_id STRING,\n" +
+                        "  event_type STRING,\n" +
+                        "  event_time TIMESTAMP(3)\n" +
+                        ") WITH (\n" +
+                        "  'connector' = 'jdbc',\n" +
+                        "  'driver' = 'com.mysql.cj.jdbc.Driver',\n" +
+                        "  'url' = 'jdbc:mysql://localhost:3306/demo',\n" +
+                        "  'table-name' = 'user_events',\n" +
+                        "  'username' = 'root',\n" +
+                        "  'password' = '12345678',\n" +
+                        "  'scan.fetch-size' = '10000'\n" +
+                        ")"
+        );
+
+        // 查询 & 更新
+        tableEnv.executeSql(
+                "SELECT * FROM user_events where id = 6"
+        ).print();
     }
 }
